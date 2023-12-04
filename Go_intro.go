cb61632a0eceb4419038b326cb5fdd3cedf981b7
Priyanshu Shukla <136368351+kloud47@users.noreply.github.com>
2023-12-05 01:28:03 +0530

    Add files via upload

---
+++ Go_intro.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Priyanshu.")
+
+	var name1 string = "ok"
+
+	name := "Priyanshu" // shortHand notation:
+
+	fmt.Println(name1)
+	fmt.Println(name)
+}
